cbf8ac05915d171341b79cc9a4c69f418e16a4c9
srsato <sera.martin@gmail.com>
2021-11-23 15:21:31 +0100

    recupero utilidades a la principal

--- sera_modulo3_ejercicio16.py
+++ sera_modulo3_ejercicio16.py
@@ -5,6 +5,9 @@ bd={"admin":"1234","sera":"password"}
 
 while True:
     print("Sistema de Login.")
+    salir=input("¿SALIR?(s): ")
+    if salir=="s":
+        break
     usuario=input("Usuario: ")
     if usuario in bd:
         pwd=input("Contraseña:")
